ce093411a3625d7621aa60071ff4300ad12b0dc5
BIMOUW <bimonurika@gmail.com>
2025-06-18 10:52:35 +0700

    update

---
+++ MonPDReborn/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPDReborn.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
