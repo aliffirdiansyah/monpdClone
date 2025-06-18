50365518794a21599addcd4287323b1250679551
BIMOUW <bimonurika@gmail.com>
2025-06-18 10:39:42 +0700

    update

--- MonPDReborn/Program.cs
+++ MonPDReborn/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+builder.Services
+    .AddControllersWithViews()
+    .AddRazorRuntimeCompilation()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 var app = builder.Build();
 
