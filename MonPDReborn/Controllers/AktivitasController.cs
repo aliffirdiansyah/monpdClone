442afffe741913d8679ab1af6058a84dd53a32d2
RaflyRoyan <royanrafly@gmail.com>
2025-06-20 08:43:47 +0700

    UI

--- MonPDReborn/Controllers/AktivitasController.cs
+++ MonPDReborn/Controllers/AktivitasController.cs
@@ -4,9 +4,21 @@ namespace MonPDReborn.Controllers
 {
     public class AktivitasController : Controller
     {
-        public IActionResult Index()
+        public IActionResult PemasanganAlat()
         {
-            return View();
+            return View("PemasanganAlat/Index");
         }
+
+        public IActionResult PemeriksaanPajak()
+        {
+            return View("PemeriksaanPajak/Index");
+        }
+        // Contoh lainnya jika ada view lain di folder Pemeriksaan/Restoran
+        public IActionResult PendataanObjek()
+        {
+            return View("PendataanObjek/Index");
+        }
+
+        
     }
 }
---
+++ MonPDReborn/Controllers/DataController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPDReborn.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult ProfilOp()
+        {
+            return View("ProfilOp/Index");
+        }
+
+        public IActionResult ProfilPembayaran()
+        {
+            return View("ProfilPembayaran/Index");
+        }
+
+        public IActionResult ProfilPotensi()
+        {
+            return View("ProfilPotensi/Index");
+        }
+
+        public IActionResult ProfilSpasial()
+        {
+            return View("ProfilSpasial/Index");
+        }
+
+        public IActionResult ProfilTarget()
+        {
+            return View("ProfilTarget/Index");
+        }
+        public IActionResult Pelaporan()
+        {
+            return View("Pelaporan/Index");
+        }
+        public IActionResult Penetapan()
+        {
+            return View("Penetapan/Index");
+        }
+        public IActionResult Pencarian()
+        {
+            return View("Pencarian/Index");
+        }
+    }
+}
