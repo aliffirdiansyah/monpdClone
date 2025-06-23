981a4ca9671a23be67eea4c55436fb3e676e45b3
NajibId97 <najibijab@gmail.com>
2025-06-23 16:17:14 +0700

    update montoringGlobal Tahunan

---
+++ MonPDReborn/Models/MonitoringGlobal/MonitoringTahunanVM.cs
@@ -0,0 +1,33 @@
+﻿namespace MonPDReborn.Models.MonitoringGlobal
+{
+    public class MonitoringTahunanVM
+    {
+        public class Index
+        {
+            public DateTime TanggalCutOff { get; set; } = DateTime.Now;
+            public Index()
+            {
+
+            }
+        }
+        public class Show
+        {
+            public List<MonitoringTahunanViewModels.TahunanPajak> TahunanPajakList { get; set; } = new();
+            public Show(DateTime TanggalCutOff)
+            {
+                
+            }
+        }
+        public class MonitoringTahunanViewModels
+        {
+            public class TahunanPajak
+            {
+                public string JenisPajak { get; set; } = null!;
+                public long AkpTahun { get; set; }
+                public long RealisasiHariAccrual { get; set; }
+                public long RealisasiSDHariAccrual { get; set; }
+                public double PersenAccrual { get; set; }
+            }
+        }
+    }
+}
