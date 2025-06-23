7b155f098c8f49573914f594648bc473bbe1c33b
RaflyRoyan <royanrafly@gmail.com>
2025-06-23 10:07:11 +0700

    Aktivitas

---
+++ MonPDReborn/Controllers/PemeriksaanPajak/PemeriksaanPajakController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPDReborn.Controllers.Pemeriksaan
+{
+    public class PemeriksaanPajakController : Controller
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<PemeriksaanPajakController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        public PemeriksaanPajakController(ILogger<PemeriksaanPajakController> logger)
+        {
+            URLView = string.Concat("../Aktivitas/PemeriksaanPajak/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var model = new Models.Pemeriksaan.PemeriksaanPajak.Index();
+                return PartialView($"{URLView}{actionName}", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IActionResult Show(string keyword)
+        {
+            try
+            {
+                var model = new Models.Pemeriksaan.PemeriksaanPajak.Show(keyword);
+                return PartialView("~/Views/Aktivitas/PemeriksaanPajak/_Show.cshtml", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IActionResult Detail(string keyword)
+        {
+            try
+            {
+                var model = new Models.Pemeriksaan.PemeriksaanPajak.Detail(keyword);
+                return PartialView("~/Views/Aktivitas/PemeriksaanPajak/_Detail.cshtml", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ MonPDReborn/Controllers/PendataanObjek/PendataanObjekController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPDReborn.Controllers.PendataanObjek
+{
+    public class PendataanObjekController : Controller
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<PendataanObjekController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        public PendataanObjekController(ILogger<PendataanObjekController> logger)
+        {
+            URLView = string.Concat("../Aktivitas/PendataanObjek/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                var model = new Models.PendataanObjek.PendataanObjek.Index();
+                return PartialView($"{URLView}{actionName}", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IActionResult Show(string keyword)
+        {
+            try
+            {
+                var model = new Models.PendataanObjek.PendataanObjek.Show(keyword);
+                return PartialView("~/Views/Aktivitas/PendataanObjek/_Show.cshtml", model);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IActionResult Detail(string keyword)
+        {
+            try
+            {
+                var model = new Models.PendataanObjek.PendataanObjek.Detail(keyword);
+                return PartialView("~/Views/Aktivitas/PendataanObjek/_Detail.cshtml", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
