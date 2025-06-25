131ed4e00ba98c5ffd4aee707abf29b07dbf0469
dotahaggard <dotahaggard@gmail.com>
2025-06-25 09:22:44 +0700

    gg

---
+++ MonPDLib/BimaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonPDLib
+{
+    public class BimaContext : DbContext
+    {
+        public BimaContext()
+        {
+        }
+
+        public BimaContext(DbContextOptions<BimaContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
