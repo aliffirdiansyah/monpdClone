7fff761d7c65b494cc5c687ce524f4b0c55f76c6
gethu <gethu@GETHUK>
2025-07-01 15:51:25 +0700

    Ubah Harian

---
+++ MonPDReborn/Controllers/MonitoringGlobal/MonitoringHarianController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPDReborn.Controllers.MonitoringGlobal
+{
+    public class MonitoringHarianController : Controller
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<MonitoringHarianController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        const string TD_KEY = "TD_KEY";
+        const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
+        public MonitoringHarianController(ILogger<MonitoringHarianController> logger)
+        {
+            URLView = string.Concat("../MonitoringGlobal/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = controllerName;
+                var model = new Models.MonitoringGlobal.MonitoringHarianVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        //public IActionResult Show(DateTime tglCutOff)
+        //{
+        //    //try
+        //    //{
+        //    //    var model = new Models.MonitoringGlobal.MonitoringHarianVM.Show(tglCutOff);
+        //    //    return PartialView($"{URLView}_{actionName}", model);
+        //    //}
+        //    //catch (Exception)
+        //    //{
+
+        //    //    throw;
+        //    //}
+        //}
+    }
+}
---
+++ MonPDReborn/Models/MonitoringGlobal/MonitoringHarianVM .cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Pdf.Native.BouncyCastle.Asn1.X509;
+
+namespace MonPDReborn.Models.MonitoringGlobal
+{
+    public class MonitoringHarianVM
+    {
+        public class Index
+        {
+
+        }
+        public class Show
+        {
+
+        }
+    }
+}
