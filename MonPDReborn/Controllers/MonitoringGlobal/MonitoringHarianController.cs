aedc67ceaa6bf7f07d4c702ad232f62a40444043
NajibId97 <najibijab@gmail.com>
2025-07-02 09:24:17 +0700

    Enhance error handling and UI in web application
    - Added new using directives and a ResponseBase instance in PencarianOPController and MonitoringHarianController for improved error handling and structured JSON responses.
    - Updated data retrieval logic in PencarianOPVM to fetch only current year records and added support for DbOpAbts.
    - Modified _Show.cshtml and Index.cshtml to include new UI elements and remove outdated sections.
    - Created a new Detail.cshtml file for a comprehensive object profile view, enhancing user experience and maintainability.

--- MonPDReborn/Controllers/MonitoringGlobal/MonitoringHarianController.cs
+++ MonPDReborn/Controllers/MonitoringGlobal/MonitoringHarianController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MonPDReborn.Lib.General;
+using static MonPDReborn.Lib.General.ResponseBase;
 
 namespace MonPDReborn.Controllers.MonitoringGlobal
 {
@@ -12,6 +14,7 @@ namespace MonPDReborn.Controllers.MonitoringGlobal
 
         const string TD_KEY = "TD_KEY";
         const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
+        public ResponseBase response = new ResponseBase();
         public MonitoringHarianController(ILogger<MonitoringHarianController> logger)
         {
             URLView = string.Concat("../MonitoringGlobal/", GetType().Name.Replace("Controller", ""), "/");
@@ -25,9 +28,17 @@ namespace MonPDReborn.Controllers.MonitoringGlobal
                 var model = new Models.MonitoringGlobal.MonitoringHarianVM.Index();
                 return View($"{URLView}{actionName}", model);
             }
-            catch (Exception)
+            catch (ArgumentException e)
             {
-                throw;
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = ex.InnerException == null ? ex.Message : ex.InnerException.Message;
+                return Json(response);
             }
         }
         //public IActionResult Show(DateTime tglCutOff)
