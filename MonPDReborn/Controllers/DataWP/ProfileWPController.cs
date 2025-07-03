8e1e2aa21ea67a5f45b60eb385ebb849b78b3eaa
NajibId97 <najibijab@gmail.com>
2025-07-03 11:43:54 +0700

    Update Foldering DataWP dan AktivitasWP

---
+++ MonPDReborn/Controllers/DataWP/ProfileWPController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPDReborn.Lib.General;
+using static MonPDReborn.Lib.General.ResponseBase;
+
+namespace MonPDReborn.Controllers.DataWP
+{
+    public class ProfileWPController : Controller
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<ProfileWPController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        const string TD_KEY = "TD_KEY";
+        const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
+        ResponseBase response = new ResponseBase();
+        public ProfileWPController(ILogger<ProfileWPController> logger)
+        {
+            URLView = string.Concat("../DataWP/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = controllerName;
+                var model = new Models.DataWP.ProfileWPVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+        public IActionResult Show(DateTime tglCutOff)
+        {
+            try
+            {
+                var model = new Models.DataWP.ProfileWPVM.Show();
+                return PartialView($"{URLView}_{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+    }
+}
