0651b03d19dbdee066efebf7f184c00b0eb57b2c
gethu <gethu@GETHUK>
2025-07-03 15:56:30 +0700

    Nambah Folder Monitoring Wilayah

---
+++ MonPDReborn/Controllers/MonitoringWilayah/KontrolWilayahController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MonPDReborn.Models.MonitoringWilayah;
+
+
+namespace MonPDReborn.Controllers.MonitoringWilayah
+{
+    public class KontrolWilayahController : Controller
+    {
+        private readonly string URLView;
+
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        public KontrolWilayahController()
+        {
+            URLView = $"../MonitoringWilayah/{GetType().Name.Replace("Controller", "")}/";
+        }
+
+        public ActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = controllerName;
+                var model = new Models.MonitoringWilayah.MonitoringWilayahVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ MonPDReborn/Models/MonitoringWilayah/MonitoringWilayahVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MonPDReborn.Models.MonitoringWilayah
+{
+    public class MonitoringWilayahVM
+    {
+        public class Index
+        {
+
+        }
+
+        public class Show
+        {
+            // Opsional jika ingin tampilan per detail wilayah-harian
+        }
+    }
+}
