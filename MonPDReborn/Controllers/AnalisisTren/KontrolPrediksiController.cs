fd0da633f5ee76ce9c8a82f434eade08ef9143c9
gethu <gethu@GETHUK>
2025-07-08 13:47:30 +0700

    Ubah

--- MonPDReborn/Controllers/AnalisisTren/KontrolPrediksiController.cs
+++ MonPDReborn/Controllers/AnalisisTren/KontrolPrediksiController.cs
@@ -1,36 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MonPDReborn.Models.AktivitasOP;
 using MonPDReborn.Models.AnalisisTren.KontrolPrediksiVM;
 
 namespace MonPDReborn.Controllers.AnalisisTren
 {
     public class KontrolPrediksiController : Controller
     {
-        private readonly string URLView;
         private readonly ILogger<KontrolPrediksiController> _logger;
+        private readonly string URLView;
 
         private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
         private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
 
-        const string TD_KEY = "TD_KEY";
-        const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
-
         public KontrolPrediksiController(ILogger<KontrolPrediksiController> logger)
         {
-            URLView = string.Concat("../AnalisisTren/", nameof(KontrolPrediksiController).Replace("Controller", ""), "/");
             _logger = logger;
+            URLView = "../AnalisisTren/KontrolPrediksi/";
         }
 
         public IActionResult Index()
         {
             try
             {
-                ViewData["Title"] = controllerName;
-                var model = new 
-                {
-                    Keyword = HttpContext.Session.GetString(TD_KEY) ?? string.Empty
-                };
+                var model = new MonPDReborn.Models.AnalisisTren.KontrolPrediksiVM.Index();
+
                 return PartialView($"{URLView}{actionName}", model);
             }
             catch (Exception ex)
@@ -44,15 +37,7 @@ namespace MonPDReborn.Controllers.AnalisisTren
         {
             try
             {
-                var model = new Models.AnalisisTren.KontrolPrediksiVM.Show(keyword)
-                {
-                    Data = new List<Models.AnalisisTren.KontrolPrediksiVM.DataRealisasiPrediksi>
-                    {
-                        new() { JenisPajak = "Hotel", Target = 1000000000, Realisasi = 800000000, Prediksi = 950000000, Potensi = 1200000000, Tren = Models.AnalisisTren.KontrolPrediksiVM.TrendStatus.Naik, Aksi = "<button class='btn btn-sm btn-primary'>Detail</button>" },
-                        new() { JenisPajak = "Restoran", Target = 1200000000, Realisasi = 700000000, Prediksi = 850000000, Potensi = 1300000000, Tren = Models.AnalisisTren.KontrolPrediksiVM.TrendStatus.Turun, Aksi = "<button class='btn btn-sm btn-primary'>Detail</button>" }
-                    }
-                };
-
+                var model = new Show(); // Message: Hello World from Show!
                 return PartialView($"{URLView}_{actionName}", model);
             }
             catch (Exception ex)
--- MonPDReborn/Models/AnalisisTren/KontrolPrediksiVM.cs
+++ MonPDReborn/Models/AnalisisTren/KontrolPrediksiVM.cs
@@ -2,31 +2,11 @@
 {
     public class Index
     {
-        public string? Keyword { get; set; }
+        public string Message { get; set; } = "Hello World from Index!";
     }
 
     public class Show
     {
-        public Show(string keyword) => Keyword = keyword;
-        public string Keyword { get; set; }
-        public List<DataRealisasiPrediksi> Data { get; set; } = new();
-    }
-
-    public class DataRealisasiPrediksi
-    {
-        public string JenisPajak { get; set; }
-        public decimal Target { get; set; }
-        public decimal Realisasi { get; set; }
-        public decimal Prediksi { get; set; }
-        public decimal Potensi { get; set; }
-        public TrendStatus Tren { get; set; }
-        public string Aksi { get; set; }
-    }
-
-    public enum TrendStatus
-    {
-        Naik,
-        Turun,
-        Stabil
+        public string Message { get; set; } = "Hello World from Show!";
     }
 }
