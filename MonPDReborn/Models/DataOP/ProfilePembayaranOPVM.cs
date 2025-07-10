345eabc3c764351f989d66fb6888400c1b594db2
RaflyRoyan <royanrafly@gmail.com>
2025-07-10 13:47:14 +0700

    update

--- MonPDReborn/Models/DataOP/ProfilePembayaranOPVM.cs
+++ MonPDReborn/Models/DataOP/ProfilePembayaranOPVM.cs
@@ -1,10 +1,11 @@
 ﻿using ClosedXML.Excel;
+using Microsoft.EntityFrameworkCore;
 using MonPDLib;
 using MonPDLib.General;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq; // diperlukan untuk .Select()
+using System.Web.Mvc;
 
 namespace MonPDReborn.Models.DataOP
 {
@@ -12,101 +13,180 @@ namespace MonPDReborn.Models.DataOP
     {
         public class Index
         {
-            //public IdentitasObjekPajak IdentitasPajak { get; set; }
+            public string Keyword { get; set; } = string.Empty;
+            public int SelectedPajak { get; set; }
 
-            //public MasaPajak MasaPajakData { get; set; } // PASTIKAN PUBLIC
+            public List<SelectListItem> JenisPajakList { get; set; } = new();
 
-            //public MasaBayar MasaBayarData { get; set; } // PASTIKAN PUBLIC
-
-            //public Index()
-            //{
-            //    IdentitasPajak = new IdentitasObjekPajak
-            //    {
-            //        NOP = "35.78.120.120.0001.0",
-            //        NamaObjekPajak = "Hotel Adam",
-            //        AlamatLengkap = "Jl. Ada",
-            //        Kecamatan_Kelurahan = "Ada - Ada"
-            //    };
-
-            //    var dataRealisasiBulanan = new List<DataRealisasiPajak>
-            //    {
-            //        new DataRealisasiPajak { Bulan = "Januari", TglSSPD = new DateTime(2025, 1, 15), Realisasi = 1000000m },
-            //        new DataRealisasiPajak { Bulan = "Februari", TglSSPD = new DateTime(2025, 2, 15), Realisasi = 1200000m },
-            //        new DataRealisasiPajak { Bulan = "Maret", TglSSPD = new DateTime(2025, 3, 15), Realisasi = 1300000m },
-            //        new DataRealisasiPajak { Bulan = "April", TglSSPD = new DateTime(2025, 4, 15), Realisasi = 1250000m },
-            //        new DataRealisasiPajak { Bulan = "Mei", TglSSPD = new DateTime(2025, 5, 15), Realisasi = 1400000m },
-            //        new DataRealisasiPajak { Bulan = "Juni", TglSSPD = new DateTime(2025, 6, 15), Realisasi = 1100000m },
-            //        new DataRealisasiPajak { Bulan = "Juli", TglSSPD = new DateTime(2025, 7, 15), Realisasi = 1500000m },
-            //        new DataRealisasiPajak { Bulan = "Agustus", TglSSPD = new DateTime(2025, 8, 15), Realisasi = 1600000m },
-            //        new DataRealisasiPajak { Bulan = "September", TglSSPD = new DateTime(2025, 9, 15), Realisasi = 1700000m },
-            //        new DataRealisasiPajak { Bulan = "Oktober", TglSSPD = new DateTime(2025, 10, 15), Realisasi = 1800000m },
-            //        new DataRealisasiPajak { Bulan = "November", TglSSPD = new DateTime(2025, 11, 15), Realisasi = 1900000m },
-            //        new DataRealisasiPajak { Bulan = "Desember", TglSSPD = new DateTime(2025, 12, 15), Realisasi = 2000000m }
-            //    };
-
-
-            //    MasaPajakData = new MasaPajak
-            //    {
-            //        Tahun = 2025,
-            //        DataRealisasi = dataRealisasiBulanan.Select(d => new DataRealisasiPajak
-            //        {
-            //            Bulan = d.Bulan,
-            //            TglSSPD = d.TglSSPD,
-            //            Realisasi = d.Realisasi
-            //        }).ToList(),
-            //        Total = dataRealisasiBulanan.Sum(d => d.Realisasi)
-            //    };
-
-
-            //    var dataRealisasiBayar = new List<DataRealisasiPajak>
-            //    {
-            //        new DataRealisasiPajak { Bulan = "Januari", TglSSPD = new DateTime(2025, 2, 10), Realisasi = 950000m },
-            //        new DataRealisasiPajak { Bulan = "Februari", TglSSPD = new DateTime(2025, 3, 12), Realisasi = 1150000m },
-            //        new DataRealisasiPajak { Bulan = "Maret", TglSSPD = new DateTime(2025, 4, 9), Realisasi = 1280000m },
-            //        new DataRealisasiPajak { Bulan = "April", TglSSPD = new DateTime(2025, 5, 15), Realisasi = 1225000m },
-            //        new DataRealisasiPajak { Bulan = "Mei", TglSSPD = new DateTime(2025, 6, 17), Realisasi = 1380000m },
-            //        new DataRealisasiPajak { Bulan = "Juni", TglSSPD = new DateTime(2025, 7, 10), Realisasi = 1050000m },
-            //        new DataRealisasiPajak { Bulan = "Juli", TglSSPD = new DateTime(2025, 8, 13), Realisasi = 1450000m },
-            //        new DataRealisasiPajak { Bulan = "Agustus", TglSSPD = new DateTime(2025, 9, 11), Realisasi = 1580000m },
-            //        new DataRealisasiPajak { Bulan = "September", TglSSPD = new DateTime(2025, 10, 14), Realisasi = 1650000m },
-            //        new DataRealisasiPajak { Bulan = "Oktober", TglSSPD = new DateTime(2025, 11, 10), Realisasi = 1750000m },
-            //        new DataRealisasiPajak { Bulan = "November", TglSSPD = new DateTime(2025, 12, 9), Realisasi = 1850000m },
-            //        new DataRealisasiPajak { Bulan = "Desember", TglSSPD = new DateTime(2026, 1, 10), Realisasi = 1950000m }
-            //    };
-
-            //    MasaBayarData = new MasaBayar
-            //    {
-            //        Tahun = 2025,
-            //        DataRealisasi = dataRealisasiBayar.Select(d => new DataRealisasiPajak
-            //        {
-            //            Bulan = d.Bulan,
-            //            TglSSPD = d.TglSSPD,
-            //            Realisasi = d.Realisasi
-            //        }).ToList(),
-            //        Total = dataRealisasiBayar.Sum(d => d.Realisasi)
-            //    };
-            //}
+            public Index(string keyword)
+            {
+                JenisPajakList = Enum.GetValues(typeof(EnumFactory.EPajak))
+                   .Cast<EnumFactory.EPajak>()
+                   .Select(x => new SelectListItem
+                   {
+                       Value = ((int)x).ToString(),
+                       Text = x.GetDescription()
+                   }).ToList();
+                Keyword = keyword;
+            }
         }
+
 
 
         public class Show
         {
-            // Tambahkan properti atau method jika diperlukan
+            public IdentitasObjekPajak DataIdentitasObjekPajak { get; set; }
+            public EnumFactory.EPajak JenisPajak { get; set; }
+
+            public Show() { }
+
+            public Show(EnumFactory.EPajak jenisPajak, string keyword)
+            {
+                JenisPajak = jenisPajak;
+
+                // Ini penting: keyword WAJIB dikirim ke method
+                DataIdentitasObjekPajak = Method.GetDataPembayaranOPList(jenisPajak, keyword);
+            }
         }
+
+
+
+
+
 
         public class Detail
         {
-            // Tambahkan properti atau method jika diperlukan
+            public string NOP { get; set; }
+            public EnumFactory.EPajak JenisPajak { get; set; }
+
+            public int TahunKiri { get; set; }
+            public int TahunKanan { get; set; }
+
+            public DataTahunPembayaran DataRealisasiKiri { get; set; }
+            public DataTahunPembayaran DataRealisasiKanan { get; set; }
+
+            public Detail() { }
+
+            public Detail(EnumFactory.EPajak jenisPajak, string nop, int tahunKiri, int tahunKanan)
+            {
+                JenisPajak = jenisPajak;
+                NOP = nop;
+
+                TahunKiri = tahunKiri;
+                TahunKanan = tahunKanan;
+
+                DataRealisasiKiri = Method.GetDataTahunanPembayaranList(jenisPajak, nop, tahunKiri);
+                DataRealisasiKanan = Method.GetDataTahunanPembayaranList(jenisPajak, nop, tahunKanan);
+            }
         }
+
 
         public class Method
         {
             public static IdentitasObjekPajak GetDataPembayaranList(EnumFactory.EPajak jenisPajak, string keyword)
             {
+                if (string.IsNullOrWhiteSpace(keyword))
+                {
+                    throw new ArgumentException("Keyword harus diisi");
+                }
+                if (keyword.Length < 3)
+                {
+                    throw new ArgumentException("Keyword harus diisi minimal 3");
+                }
+
                 var ret = new IdentitasObjekPajak();
                 var currentYear = DateTime.Now.Year;
                 var context = DBClass.GetContext();
 
+                switch (jenisPajak)
+                {
+                    case EnumFactory.EPajak.MakananMinuman:
+                        var resto = context.DbOpRestos
+                            .Where(x => (x.Nop == keyword) || x.NamaOp.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (resto != null) MapIdentitas(ret, resto.Nop, resto.AlamatOp, resto.AlamatOpKdCamat, resto.AlamatOpKdLurah, resto.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.TenagaListrik:
+                        var listrik = context.DbOpListriks
+                            .Where(x => (x.Nop == keyword) || x.NamaOp.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (listrik != null) MapIdentitas(ret, listrik.Nop, listrik.AlamatOp, listrik.AlamatOpKdCamat, listrik.AlamatOpKdLurah, listrik.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.JasaPerhotelan:
+                        var hotel = context.DbOpHotels
+                            .Where(x => (x.Nop == keyword) || x.NamaOp.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (hotel != null) MapIdentitas(ret, hotel.Nop, hotel.AlamatOp, hotel.AlamatOpKdCamat, hotel.AlamatOpKdLurah, hotel.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.JasaParkir:
+                        var parkir = context.DbOpParkirs
+                            .Where(x => (x.Nop == keyword) || x.NamaOp.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (parkir != null) MapIdentitas(ret, parkir.Nop, parkir.AlamatOp, parkir.AlamatOpKdCamat, parkir.AlamatOpKdLurah, parkir.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.JasaKesenianHiburan:
+                        var hiburan = context.DbOpHiburans
+                            .Where(x => (x.Nop == keyword) || x.NamaOp.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (hiburan != null) MapIdentitas(ret, hiburan.Nop, hiburan.AlamatOp, hiburan.AlamatOpKdCamat, hiburan.AlamatOpKdLurah, hiburan.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.AirTanah:
+                        var abt = context.DbOpAbts
+                            .Where(x => (x.Nop == keyword) || x.NamaOp.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (abt != null) MapIdentitas(ret, abt.Nop, abt.AlamatOp, abt.AlamatOpKdCamat, abt.AlamatOpKdLurah, abt.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.Reklame:
+                        // Masih kosong, bisa isi kalau datanya siap
+                        break;
+
+                    case EnumFactory.EPajak.PBB:
+                        var pbb = context.DbOpPbbs
+                            .Where(x => (x.Nop == keyword) || x.WpNama.ToUpper().Contains(keyword.ToUpper()))
+                            .FirstOrDefault(x => x.TahunBuku == currentYear);
+                        if (pbb != null) MapIdentitas(ret, pbb.Nop, pbb.AlamatOp, pbb.AlamatKdCamat, pbb.AlamatKdLurah, pbb.WilayahPajak);
+                        break;
+
+                    case EnumFactory.EPajak.BPHTB:
+                    case EnumFactory.EPajak.OpsenPkb:
+                    case EnumFactory.EPajak.OpsenBbnkb:
+                        // Belum ada implementasi
+                        break;
+
+                    default:
+                        break;
+                }
+
+                return ret;
+            }
+            private static void MapIdentitas(
+            IdentitasObjekPajak ret,
+            string nop,
+            string alamatLengkap,
+            string kecamatan,
+            string kelurahan,
+            string wilayah)
+            {
+                ret.NOP = nop;
+                ret.AlamatLengkap = alamatLengkap;
+                ret.Kecamatan = kecamatan;
+                ret.Kelurahan = kelurahan;
+                ret.WilayahPajak = string.IsNullOrEmpty(wilayah) ? wilayah : wilayah;
+            }
+
+
+            public static IdentitasObjekPajak GetDataPembayaranOPList(EnumFactory.EPajak jenisPajak, string keyword)
+            {
+                var ret = new IdentitasObjekPajak();
+                var currentYear = DateTime.Now.Year;
+                var context = DBClass.GetContext();
+                
                 switch (jenisPajak)
                 {
                     case EnumFactory.EPajak.MakananMinuman:
@@ -115,10 +195,11 @@ namespace MonPDReborn.Models.DataOP
                         if (RestoTahunIni != null)
                         {
                             ret.NOP = RestoTahunIni.Nop;
+                            ret.NamaObjekPajak = RestoTahunIni.NamaOp;
                             ret.AlamatLengkap = RestoTahunIni.AlamatOp;
                             ret.Kecamatan = RestoTahunIni.AlamatOpKdCamat;
                             ret.Kelurahan = RestoTahunIni.AlamatOpKdLurah;
-                            ret.WilayahPajak = RestoTahunIni.AlamatOp;
+                            ret.WilayahPajak = RestoTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.TenagaListrik:
@@ -127,10 +208,11 @@ namespace MonPDReborn.Models.DataOP
                         if (ListrikTahunIni != null)
                         {
                             ret.NOP = ListrikTahunIni.Nop;
+                            ret.NamaObjekPajak = ListrikTahunIni.NamaOp;
                             ret.AlamatLengkap = ListrikTahunIni.AlamatOp;
                             ret.Kecamatan = ListrikTahunIni.AlamatOpKdCamat;
                             ret.Kelurahan = ListrikTahunIni.AlamatOpKdLurah;
-                            ret.WilayahPajak = ListrikTahunIni.AlamatOp;
+                            ret.WilayahPajak = ListrikTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.JasaPerhotelan:
@@ -139,10 +221,11 @@ namespace MonPDReborn.Models.DataOP
                         if (HotelTahunIni != null)
                         {
                             ret.NOP = HotelTahunIni.Nop;
+                            ret.NamaObjekPajak = HotelTahunIni.NamaOp;
                             ret.AlamatLengkap = HotelTahunIni.AlamatOp;
                             ret.Kecamatan = HotelTahunIni.AlamatOpKdCamat;
                             ret.Kelurahan = HotelTahunIni.AlamatOpKdLurah;
-                            ret.WilayahPajak = HotelTahunIni.AlamatOp;
+                            ret.WilayahPajak = HotelTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.JasaParkir:
@@ -151,10 +234,11 @@ namespace MonPDReborn.Models.DataOP
                         if (ParkirTahunIni != null)
                         {
                             ret.NOP = ParkirTahunIni.Nop;
+                            ret.NamaObjekPajak = ParkirTahunIni.NamaOp;
                             ret.AlamatLengkap = ParkirTahunIni.AlamatOp;
                             ret.Kecamatan = ParkirTahunIni.AlamatOpKdCamat;
                             ret.Kelurahan = ParkirTahunIni.AlamatOpKdLurah;
-                            ret.WilayahPajak = ParkirTahunIni.AlamatOp;
+                            ret.WilayahPajak = ParkirTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.JasaKesenianHiburan:
@@ -163,10 +247,11 @@ namespace MonPDReborn.Models.DataOP
                         if (HiburanTahunIni != null)
                         {
                             ret.NOP = HiburanTahunIni.Nop;
+                            ret.NamaObjekPajak = HiburanTahunIni.NamaOp;
                             ret.AlamatLengkap = HiburanTahunIni.AlamatOp;
                             ret.Kecamatan = HiburanTahunIni.AlamatOpKdCamat;
                             ret.Kelurahan = HiburanTahunIni.AlamatOpKdLurah;
-                            ret.WilayahPajak = HiburanTahunIni.AlamatOp;
+                            ret.WilayahPajak = HiburanTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.AirTanah:
@@ -175,10 +260,11 @@ namespace MonPDReborn.Models.DataOP
                         if (AbtTahunIni != null)
                         {
                             ret.NOP = AbtTahunIni.Nop;
+                            ret.NamaObjekPajak = AbtTahunIni.NamaOp;
                             ret.AlamatLengkap = AbtTahunIni.AlamatOp;
                             ret.Kecamatan = AbtTahunIni.AlamatOpKdCamat;
                             ret.Kelurahan = AbtTahunIni.AlamatOpKdLurah;
-                            ret.WilayahPajak = AbtTahunIni.AlamatOp;
+                            ret.WilayahPajak = AbtTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.Reklame:
@@ -190,7 +276,7 @@ namespace MonPDReborn.Models.DataOP
                         //    ret.AlamatLengkap = RekalmeTahunIni.AlamatOp;
                         //    ret.Kecamatan = RekalmeTahunIni.AlamatOpKdCamat;
                         //    ret.Kelurahan = RekalmeTahunIni.AlamatOpKdLurah;
-                        //    ret.WilayahPajak = RekalmeTahunIni.AlamatOp;
+                        //    ret.WilayahPajak = RekalmeTahunIni.WilayahPajak;
                         //}
                         break;
                     case EnumFactory.EPajak.PBB:
@@ -199,10 +285,11 @@ namespace MonPDReborn.Models.DataOP
                         if (PbbTahunIni != null)
                         {
                             ret.NOP = PbbTahunIni.Nop;
+                            ret.NamaObjekPajak = PbbTahunIni.WpNama;
                             ret.AlamatLengkap = PbbTahunIni.AlamatOp;
                             ret.Kecamatan = PbbTahunIni.AlamatKdCamat;
                             ret.Kelurahan = PbbTahunIni.AlamatKdLurah;
-                            ret.WilayahPajak = PbbTahunIni.AlamatOp;
+                            ret.WilayahPajak = PbbTahunIni.WilayahPajak;
                         }
                         break;
                     case EnumFactory.EPajak.BPHTB:
