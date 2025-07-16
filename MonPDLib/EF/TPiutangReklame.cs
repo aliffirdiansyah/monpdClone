e22f8554367e60de6663bdd647e4bee232575f0e
NajibId97 <najibijab@gmail.com>
2025-07-16 11:14:55 +0700

    Add Model Piutang isi Data pada tabel

---
+++ MonPDLib/EF/TPiutangReklame.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MonPDLib.EF;
+
+[PrimaryKey("TahunBuku", "Nop", "MasaPajak", "TahunPajak")]
+[Table("T_PIUTANG_REKLAME")]
+public partial class TPiutangReklame
+{
+    [Key]
+    [Column("TAHUN_BUKU")]
+    [Precision(4)]
+    public byte TahunBuku { get; set; }
+
+    [Key]
+    [Column("NOP")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Nop { get; set; } = null!;
+
+    [Key]
+    [Column("MASA_PAJAK")]
+    [Precision(2)]
+    public byte MasaPajak { get; set; }
+
+    [Key]
+    [Column("TAHUN_PAJAK")]
+    [Precision(4)]
+    public byte TahunPajak { get; set; }
+
+    [Column("NAMA_OP")]
+    [StringLength(150)]
+    [Unicode(false)]
+    public string NamaOp { get; set; } = null!;
+
+    [Column("ALAMAT_OP")]
+    [StringLength(150)]
+    [Unicode(false)]
+    public string AlamatOp { get; set; } = null!;
+
+    [Column("KECAMATAN_OP")]
+    [StringLength(150)]
+    [Unicode(false)]
+    public string KecamatanOp { get; set; } = null!;
+
+    [Column("KELURAHAN_OP")]
+    [StringLength(150)]
+    [Unicode(false)]
+    public string KelurahanOp { get; set; } = null!;
+
+    [Column("WILAYAH_PAJAK")]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string WilayahPajak { get; set; } = null!;
+
+    [Column("UPTB", TypeName = "NUMBER")]
+    public decimal Uptb { get; set; }
+
+    [Column("NPWPD")]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string Npwpd { get; set; } = null!;
+
+    [Column("NAMA_WP")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string NamaWp { get; set; } = null!;
+
+    [Column("ALAMAT_WP")]
+    [StringLength(150)]
+    [Unicode(false)]
+    public string AlamatWp { get; set; } = null!;
+
+    [Column("PIUTANG", TypeName = "NUMBER(15,2)")]
+    public decimal Piutang { get; set; }
+}
---
+++ MonPDLib/EF/TempPeriksa.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MonPDLib.EF;
+
+[Keyless]
+[Table("TEMP_PERIKSA")]
+public partial class TempPeriksa
+{
+    [Column("NOP")]
+    [StringLength(25)]
+    [Unicode(false)]
+    public string? Nop { get; set; }
+
+    [Column("WAJIB_PAJAK")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? WajibPajak { get; set; }
+
+    [Column("ALAMAT")]
+    [StringLength(200)]
+    [Unicode(false)]
+    public string? Alamat { get; set; }
+
+    [Column("UPTB")]
+    [Precision(2)]
+    public byte? Uptb { get; set; }
+
+    [Column("NO_SP")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? NoSp { get; set; }
+
+    [Column("TGL_ST", TypeName = "DATE")]
+    public DateTime? TglSt { get; set; }
+
+    [Column("MASA_PAJAK")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? MasaPajak { get; set; }
+
+    [Column("POKOK", TypeName = "NUMBER(15,2)")]
+    public decimal? Pokok { get; set; }
+
+    [Column("DENDA", TypeName = "NUMBER(15,2)")]
+    public decimal? Denda { get; set; }
+
+    [Column("JUMLAH_KB", TypeName = "NUMBER(15,2)")]
+    public decimal? JumlahKb { get; set; }
+
+    [Column("KET")]
+    [StringLength(200)]
+    [Unicode(false)]
+    public string? Ket { get; set; }
+
+    [Column("LHP")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? Lhp { get; set; }
+
+    [Column("TGL_LHP")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? TglLhp { get; set; }
+
+    [Column("TGL_BYR", TypeName = "DATE")]
+    public DateTime? TglByr { get; set; }
+
+    [Column("TIM")]
+    [StringLength(200)]
+    [Unicode(false)]
+    public string? Tim { get; set; }
+
+    [Column("JENIS_PAJAK")]
+    [Precision(2)]
+    public byte? JenisPajak { get; set; }
+
+    [Column("TAHUN_PAJAK")]
+    [Precision(4)]
+    public byte? TahunPajak { get; set; }
+
+    [Column("DASAR")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Dasar { get; set; }
+}
