85588a86217d53afde8f78d7f4cf70032add4538
RaflyRoyan <royanrafly@gmail.com>
2025-07-17 09:28:30 +0700

    update

---
+++ MonPDReborn/Controllers/AktivitasOP/SeriesPendapatanDaerahController.cs
@@ -0,0 +1,72 @@
+﻿using DevExpress.DataAccess.Native.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MonPDLib.General;
+using MonPDReborn.Lib.General;
+using MonPDReborn.Models.AktivitasOP;
+using System;
+using static MonPDReborn.Lib.General.ResponseBase;
+
+namespace MonPDReborn.Controllers.Aktivitas
+{
+    public class SeriesPendapatanDaerahController : Controller
+    {
+        private readonly ILogger<SeriesPendapatanDaerahController> _logger;
+        ResponseBase response = new ResponseBase();
+
+
+        private string URLView => $"../AktivitasOP/{nameof(SeriesPendapatanDaerahController).Replace("Controller", "")}/";
+
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        public SeriesPendapatanDaerahController(ILogger<SeriesPendapatanDaerahController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = controllerName;
+                var model = new Models.AktivitasOP.SeriesPendapatanDaerahVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+
+        public IActionResult Show()
+        {
+            try
+            {
+                var model = new SeriesPendapatanDaerahVM.Show();
+                return PartialView($"{URLView}_Show", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+
+    }
+}
---
+++ MonPDReborn/Models/AktivitasOP/SeriesPendapatanDaerahVM.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonPDReborn.Models.AktivitasOP
+{
+    public class SeriesPendapatanDaerahVM
+    {
+        public class Index
+        {
+            public string Keyword { get; set; } = null!;
+            public Index()
+            {
+
+            }
+
+        }
+
+        public class Show
+        {
+            public Show() { }
+
+        }
+
+
+
+        public class Detail
+        {
+            public Detail()
+            {
+
+            }
+        }
+
+        public class Method
+        {
+
+
+        }
+
+        public class DataPendapatan
+        {
+            public string JenisPajak { get; set; } = null!;
+            public decimal Target1 { get; set; }
+            public decimal Target2 { get; set; }
+            public decimal Target3 { get; set; }
+            public decimal Target4 { get; set; }
+            public decimal Target5 { get; set; }
+            public decimal Realisasi1 { get; set; }
+            public decimal Realisasi2 { get; set; }
+            public decimal Realisasi3 { get; set; }
+            public decimal Realisasi4 { get; set; }
+            public decimal Realisasi5 { get; set; }
+            public decimal Persentase1 { get; set; }
+            public decimal Persentase2 { get; set; }
+            public decimal Persentase3 { get; set; }
+            public decimal Persentase4 { get; set; }
+            public decimal Persentase5 { get; set; }
+        }
+        
+    }
+}
