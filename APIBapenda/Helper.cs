aa9ce68f898a8a83fc076efec03154d558ddd65d
dotahaggard <dotahaggard@gmail.com>
2025-07-30 13:55:52 +0700

    GG

---
+++ APIBapenda/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace APIBapenda
+{
+    public class Helper
+    {
+
+        public static string ToTitleCase(string str)
+        {
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            return textInfo.ToTitleCase(str.ToLower());
+        }
+
+        public class APIResponse<T>
+        {
+            public int Code { get; set; }
+            public string Message { get; set; }
+            public T? Data { get; set; }
+        }
+
+        public class EnumClass
+        {
+            public int EnumId { get; set; }
+            public string EnumName { get; set; } = string.Empty;
+            public string EnumDescription { get; set; } = string.Empty;
+        }
+    }
+}
