0df4515e3f53dbafe172c8059f5529a13de45a91
RaflyRoyan <royanrafly@gmail.com>
2025-07-31 14:32:41 +0700

    update

---
+++ MonPDReborn/Controllers/DataOP/PotensiUploadController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPDReborn.Lib.General;
+using static MonPDReborn.Lib.General.ResponseBase;
+
+namespace MonPDReborn.Controllers.DataOP
+{
+    public class PotensiUploadController : BaseController
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<PotensiUploadController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        const string TD_KEY = "TD_KEY";
+        const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
+        ResponseBase response = new ResponseBase();
+
+        public PotensiUploadController(ILogger<PotensiUploadController> logger)
+        {
+            URLView = string.Concat("../DataOP/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = controllerName;
+                var model = new Models.DataOP.ProfilePotensiOPVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPost]
+        public IActionResult SimpanUpaya(Models.AktivitasOP.ReklameSummaryVM.GetDetailUpaya input)
+        {
+            try
+            {
+                if (input.Lampiran == null && input.Lampiran.Length <= 0)
+                {
+                    throw new ArgumentException("Lampiran tidak boleh kosong. Silahkan upload file lampiran yang sesuai.");
+                }
+                if (input.Lampiran != null && input.Lampiran.Length > 0)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        input.Lampiran.CopyTo(ms);
+                        input.Data.NewRowUpaya.Lampiran = ms.ToArray();
+                    }
+                }
+                var insert = new Models.AktivitasOP.ReklameSummaryVM.DetailUpaya.NewRow
+                {
+                    NoFormulir = input.Data.NewRowUpaya.NoFormulir,
+                    IdUpaya = input.SelectedUpaya,
+                    IdTindakan = input.SelectedTindakan,
+                    NamaPetugas = input.Data.NewRowUpaya.NamaPetugas,
+                    TglUpaya = input.Data.NewRowUpaya.TglUpaya,
+                    Lampiran = input.Data.NewRowUpaya.Lampiran,
+                };
+                Models.AktivitasOP.ReklameSummaryVM.Method.SimpanUpaya(insert);
+
+                response.Status = StatusEnum.Success;
+                response.Message = "Data Berhasil Disimpan";
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+            return Json(response);
+        }
+    }
+}
---
+++ MonPDReborn/Models/DataOP/PotensiUploadVM.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPDLib;
+using MonPDLib.EF;
+
+namespace MonPDReborn.Models.DataOP
+{
+    public class PotensiUploadVM
+    {
+        public class Index
+        {
+            public string Keyword { get; set; } = null!;
+            public IFormFile Lampiran { get; set; } = null!;
+            public Index()
+            {
+
+            }
+
+        }
+
+        public class  Method
+        {
+            public static void SimpanLampiranExcel(IFormFile fileExcel)
+            {
+                if (fileExcel == null || fileExcel.Length == 0)
+                    throw new ArgumentException("File Excel tidak boleh kosong.");
+
+                var context = DBClass.GetContext();
+
+                // Konversi file ke byte[]
+                byte[] excelData;
+                using (var ms = new MemoryStream())
+                {
+                    fileExcel.CopyTo(ms);
+                    excelData = ms.ToArray();
+                }
+
+                // Simpan ke entitas (gunakan nilai default atau placeholder jika perlu)
+                var newUpaya = new DbMonReklameUpaya
+                {
+                    Upaya = "UPLOAD EXCEL",
+                    Keterangan = "Lampiran file Excel",
+                  
+                    DbMonReklameUpayaDok = new DbMonReklameUpayaDok
+                    {
+                        Gambar = excelData
+                    }
+                };
+
+                context.DbMonReklameUpayas.Add(newUpaya);
+                context.SaveChanges();
+            }
+
+        }
+
+        public class Upload
+        {
+            public byte[] lampiran { get; set; } = null!;
+        }
+    }
+}
