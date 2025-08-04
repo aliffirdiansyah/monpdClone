d11d4110f30f75b848fb8657c2682571de42dab1
BIMOUW <bimonurika@gmail.com>
2025-08-04 11:28:26 +0700

    update dar der dor jedar jedor

---
+++ MonPDLib/EF/DbMonReklameInsJumlah.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MonPDLib.EF;
+
+[Table("DB_MON_REKLAME_INS_JUMLAH")]
+public partial class DbMonReklameInsJumlah
+{
+    [Key]
+    [Column("NO_FORMULIR")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string NoFormulir { get; set; } = null!;
+
+    [Column("JUMLAH")]
+    [Precision(10)]
+    public int Jumlah { get; set; }
+}
