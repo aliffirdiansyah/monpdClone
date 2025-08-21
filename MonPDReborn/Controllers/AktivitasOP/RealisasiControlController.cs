d2565a735a3dd88ac22d196829bf97b5e3f09576
RaflyRoyan <royanrafly@gmail.com>
2025-08-21 14:50:43 +0700

    updatteee

--- MonPDReborn/Controllers/AktivitasOP/RealisasiControlController.cs
+++ MonPDReborn/Controllers/AktivitasOP/RealisasiControlController.cs
@@ -4,7 +4,7 @@ using static MonPDReborn.Lib.General.ResponseBase;
 
 namespace MonPDReborn.Controllers.Aktivitas
 {
-    public class RealisasiControlController : BaseController
+    public class RealisasiControlController : Controller
     {
         string URLView = string.Empty;
 
