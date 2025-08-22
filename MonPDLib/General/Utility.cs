df7059727f4b873c440fa67acf59389372e9cb4b
NajibId97 <najibijab@gmail.com>
2025-08-22 10:15:50 +0700

    pushhhh login login YPTBYPB

--- MonPDLib/General/Utility.cs
+++ MonPDLib/General/Utility.cs
@@ -12,6 +12,7 @@ namespace MonPDLib.General
     public class Utility
     {
         public const string SESSION_USER = "SESSION_USER";
+        public const string SESSION_NAMA = "SESSION_NAMA";
         public const string SESSION_ROLE = "SESSION_ROLE";
 
         public class TahunModel
