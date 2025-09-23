d1503dbf15dd778ae444a15bdd2732808ba710cb
RaflyRoyan <royanrafly@gmail.com>
2025-09-23 08:59:19 +0700

    updatee

--- MonPDReborn/Models/CCTVParkir/MonitoringCCTVVM.cs
+++ MonPDReborn/Models/CCTVParkir/MonitoringCCTVVM.cs
@@ -50,16 +50,16 @@ namespace MonPDReborn.Models.CCTVParkir
             {
                 List<MonitoringCCTV> result = new()
                 {
-                    new MonitoringCCTV { Nop = "3171010001", NamaOp = "Parkir Plaza Surabaya", AlamatOp = "Jl. Pemuda No. 1", WilayahPajak = "Genteng", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-5) },
-                    new MonitoringCCTV { Nop = "3171010002", NamaOp = "Parkir Tunjungan Plaza", AlamatOp = "Jl. Basuki Rahmat No. 8", WilayahPajak = "Tegalsari", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-10) },
-                    new MonitoringCCTV { Nop = "3171010003", NamaOp = "Parkir Galaxy Mall", AlamatOp = "Jl. Dharmahusada Indah Timur", WilayahPajak = "Mulyorejo", UptbId = uptbId, StatusAktif = "Non-Aktif", TglTerakhirAktif = DateTime.Now.AddHours(-2) },
-                    new MonitoringCCTV { Nop = "3171010004", NamaOp = "Parkir Royal Plaza", AlamatOp = "Jl. Ahmad Yani No. 16", WilayahPajak = "Wonokromo", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now },
-                    new MonitoringCCTV { Nop = "3171010005", NamaOp = "Parkir Pakuwon Mall", AlamatOp = "Jl. Puncak Indah Lontar", WilayahPajak = "Sambikerep", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-30) },
-                    new MonitoringCCTV { Nop = "3171010006", NamaOp = "Parkir City of Tomorrow", AlamatOp = "Jl. Ahmad Yani No. 288", WilayahPajak = "Jambangan", UptbId = uptbId, StatusAktif = "Non-Aktif", TglTerakhirAktif = DateTime.Now.AddHours(-1) },
-                    new MonitoringCCTV { Nop = "3171010007", NamaOp = "Parkir Lenmarc", AlamatOp = "Jl. Bukit Darmo Golf", WilayahPajak = "Sukomanunggal", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now },
-                    new MonitoringCCTV { Nop = "3171010008", NamaOp = "Parkir BG Junction", AlamatOp = "Jl. Bubutan No. 1", WilayahPajak = "Bubutan", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-45) },
-                    new MonitoringCCTV { Nop = "3171010009", NamaOp = "Parkir Ciputra World", AlamatOp = "Jl. Mayjen Sungkono No. 87", WilayahPajak = "Sawahan", UptbId = uptbId, StatusAktif = "Non-Aktif", TglTerakhirAktif = DateTime.Now.AddHours(-5) },
-                    new MonitoringCCTV { Nop = "3171010010", NamaOp = "Parkir Grand City", AlamatOp = "Jl. Walikota Mustajab No. 1", WilayahPajak = "Genteng", UptbId = uptbId, StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now }
+                    new MonitoringCCTV { Nop = "3171010001", NamaOp = "Parkir Plaza Surabaya", AlamatOp = "Jl. Pemuda No. 1", WilayahPajak = "Genteng", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-5) },
+                    new MonitoringCCTV { Nop = "3171010002", NamaOp = "Parkir Tunjungan Plaza", AlamatOp = "Jl. Basuki Rahmat No. 8", WilayahPajak = "Tegalsari", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-10) },
+                    new MonitoringCCTV { Nop = "3171010003", NamaOp = "Parkir Galaxy Mall", AlamatOp = "Jl. Dharmahusada Indah Timur", WilayahPajak = "Mulyorejo", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Non-Aktif", TglTerakhirAktif = DateTime.Now.AddHours(-2) },
+                    new MonitoringCCTV { Nop = "3171010004", NamaOp = "Parkir Royal Plaza", AlamatOp = "Jl. Ahmad Yani No. 16", WilayahPajak = "Wonokromo", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now },
+                    new MonitoringCCTV {Nop = "3171010005", NamaOp = "Parkir Pakuwon Mall", AlamatOp = "Jl. Puncak Indah Lontar", WilayahPajak = "Sambikerep", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-30)},
+                    new MonitoringCCTV {Nop = "3171010006", NamaOp = "Parkir City of Tomorrow", AlamatOp = "Jl. Ahmad Yani No. 288", WilayahPajak = "Jambangan", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Non-Aktif", TglTerakhirAktif = DateTime.Now.AddHours(-1)},
+                    new MonitoringCCTV {Nop = "3171010007", NamaOp = "Parkir Lenmarc", AlamatOp = "Jl. Bukit Darmo Golf", WilayahPajak = "Sukomanunggal", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now},
+                    new MonitoringCCTV {Nop = "3171010008", NamaOp = "Parkir BG Junction", AlamatOp = "Jl. Bubutan No. 1", WilayahPajak = "Bubutan", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now.AddMinutes(-45)},
+                    new MonitoringCCTV {Nop = "3171010009", NamaOp = "Parkir Ciputra World", AlamatOp = "Jl. Mayjen Sungkono No. 87", WilayahPajak = "Sawahan", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Non-Aktif", TglTerakhirAktif = DateTime.Now.AddHours(-5)},
+                    new MonitoringCCTV {Nop = "3171010010", NamaOp = "Parkir Grand City", AlamatOp = "Jl. Walikota Mustajab No. 1", WilayahPajak = "Genteng", UptbId = uptbId, TglTerpasang = DateTime.Now.AddMinutes(-5), Vendor = "Jasmita", StatusAktif = "Aktif", TglTerakhirAktif = DateTime.Now}
                 };
                 return result;
             }
@@ -68,16 +68,16 @@ namespace MonPDReborn.Models.CCTVParkir
             {
                 return new List<MonitoringCCTVDet>
                 {
-                    new MonitoringCCTVDet { Nop = "3171010001", TglAktif = DateTime.Today.AddDays(-10), TglDown = DateTime.Today.AddDays(-9), StatusAktif = "Aktif", DownTime = 2 },
-                    new MonitoringCCTVDet { Nop = "3171010002", TglAktif = DateTime.Today.AddDays(-8), TglDown = DateTime.Today.AddDays(-7), StatusAktif = "Down", DownTime = 5 },
-                    new MonitoringCCTVDet { Nop = "3171010003", TglAktif = DateTime.Today.AddDays(-7), TglDown = DateTime.Today.AddDays(-6), StatusAktif = "Aktif", DownTime = 1 },
-                    new MonitoringCCTVDet { Nop = "3171010004", TglAktif = DateTime.Today.AddDays(-6), TglDown = DateTime.Today.AddDays(-5), StatusAktif = "Aktif", DownTime = 0 },
-                    new MonitoringCCTVDet { Nop = "3171010005", TglAktif = DateTime.Today.AddDays(-5), TglDown = DateTime.Today.AddDays(-4), StatusAktif = "Down", DownTime = 3 },
-                    new MonitoringCCTVDet { Nop = "3171010006", TglAktif = DateTime.Today.AddDays(-4), TglDown = DateTime.Today.AddDays(-3), StatusAktif = "Aktif", DownTime = 0 },
-                    new MonitoringCCTVDet { Nop = "3171010007", TglAktif = DateTime.Today.AddDays(-3), TglDown = DateTime.Today.AddDays(-2), StatusAktif = "Down", DownTime = 7 },
-                    new MonitoringCCTVDet { Nop = "3171010008", TglAktif = DateTime.Today.AddDays(-2), TglDown = DateTime.Today.AddDays(-1), StatusAktif = "Aktif", DownTime = 0 },
-                    new MonitoringCCTVDet { Nop = "3171010009", TglAktif = DateTime.Today.AddDays(-1), TglDown = DateTime.Today, StatusAktif = "Aktif", DownTime = 0 },
-                    new MonitoringCCTVDet { Nop = "3171010010", TglAktif = DateTime.Today, TglDown = DateTime.Today.AddDays(1), StatusAktif = "Aktif", DownTime = 0 }
+                    new MonitoringCCTVDet { Nop = "3171010001", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-10), TglDown = DateTime.Today.AddDays(-9), StatusAktif = "Aktif", DownTime = 2 },
+                    new MonitoringCCTVDet { Nop = "3171010002", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-8), TglDown = DateTime.Today.AddDays(-7), StatusAktif = "Down", DownTime = 5 },
+                    new MonitoringCCTVDet { Nop = "3171010003", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-7), TglDown = DateTime.Today.AddDays(-6), StatusAktif = "Aktif", DownTime = 1 },
+                    new MonitoringCCTVDet { Nop = "3171010004", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-6), TglDown = DateTime.Today.AddDays(-5), StatusAktif = "Aktif", DownTime = 0 },
+                    new MonitoringCCTVDet { Nop = "3171010005", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-5), TglDown = DateTime.Today.AddDays(-4), StatusAktif = "Down", DownTime = 3 },
+                    new MonitoringCCTVDet { Nop = "3171010006", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-4), TglDown = DateTime.Today.AddDays(-3), StatusAktif = "Aktif", DownTime = 0 },
+                    new MonitoringCCTVDet { Nop = "3171010007", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-3), TglDown = DateTime.Today.AddDays(-2), StatusAktif = "Down", DownTime = 7 },
+                    new MonitoringCCTVDet { Nop = "3171010008", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-2), TglDown = DateTime.Today.AddDays(-1), StatusAktif = "Aktif", DownTime = 0 },
+                    new MonitoringCCTVDet { Nop = "3171010009", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today.AddDays(-1), TglDown = DateTime.Today, StatusAktif = "Aktif", DownTime = 0 },
+                    new MonitoringCCTVDet { Nop = "3171010010", Tgl = DateTime.Today.AddDays(-10), TglAktif = DateTime.Today, TglDown = DateTime.Today.AddDays(1), StatusAktif = "Aktif", DownTime = 0 }
                 };
             }
 
@@ -113,11 +113,14 @@ namespace MonPDReborn.Models.CCTVParkir
             public string WilayahPajak { get; set; } = null!;
             public int UptbId { get; set; }
             public string StatusAktif { get; set; } = null!;
+            public DateTime TglTerpasang { get; set; }
+            public string Vendor { get; set; } = null!;
             public DateTime TglTerakhirAktif { get; set; }
         }
         public class MonitoringCCTVDet
         {
             public string Nop { get; set; } = null!;
+            public DateTime Tgl { get; set; }
             public DateTime TglAktif { get; set; }
             public DateTime TglDown { get; set; }
             public string StatusAktif { get; set; } = null!;
