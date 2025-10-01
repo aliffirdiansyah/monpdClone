0e1720e5ede0c66902887c5eeaf132799d77d6e9
RaflyRoyan <royanrafly@gmail.com>
2025-10-01 10:36:22 +0700

    savlkn

--- MonPDLib/EF/DbAkunTargetObjekReklame.cs
+++ MonPDLib/EF/DbAkunTargetObjekReklame.cs
@@ -9,27 +9,21 @@ namespace MonPDLib.EF;
 [Keyless]
 public partial class DbAkunTargetObjekReklame
 {
-    [Column("KATEGORI_ID", TypeName = "NUMBER")]
-    public decimal? KategoriId { get; set; }
+    [Column("NO_FORMULIR")]
+    [StringLength(20)]
+    [Unicode(false)]
+    public string? NoFormulir { get; set; }
 
     [Column("FLAG_PERMOHONAN")]
     [StringLength(10)]
     [Unicode(false)]
     public string? FlagPermohonan { get; set; }
 
+    [Column("KATEGORI_ID", TypeName = "NUMBER")]
+    public decimal? KategoriId { get; set; }
+
     [Column("TAHUN_BUKU", TypeName = "NUMBER")]
-    public decimal? TahunBuku { get; set; }
-
-    [Column("BULAN_BUKU", TypeName = "NUMBER")]
-    public decimal? BulanBuku { get; set; }
-
-    [Column("NOR")]
-    [StringLength(100)]
-    [Unicode(false)]
-    public string? Nor { get; set; }
-
-    [Column("TARGET", TypeName = "NUMBER")]
-    public decimal? Target { get; set; }
+    public decimal TahunBuku { get; set; }
 
     [Column("TARGET_BULAN", TypeName = "NUMBER")]
     public decimal? TargetBulan { get; set; }
