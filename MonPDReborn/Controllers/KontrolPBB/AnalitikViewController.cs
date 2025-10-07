5f50b96e7f99aac3a44076a068905f20432bacae
RaflyRoyan <royanrafly@gmail.com>
2025-10-07 13:39:03 +0700

    update

---
+++ MonPDReborn/Controllers/KontrolPBB/AnalitikViewController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPDLib.General;
+using MonPDReborn.Lib.General;
+
+namespace MonPDReborn.Controllers.KontrolPBB
+{
+    public class AnalitikViewController : BaseController
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<AnalitikViewController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        const string TD_KEY = "TD_KEY";
+        const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
+        ResponseBase response = new ResponseBase();
+        public AnalitikViewController(ILogger<AnalitikViewController> logger)
+        {
+            URLView = string.Concat("../KontrolPBB/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = "Dashboard Analitik";
+                var nama = HttpContext.Session.GetString(Utility.SESSION_NAMA).ToString();
+                if (string.IsNullOrEmpty(nama))
+                {
+                    throw new ArgumentException("Session tidak ditemukan dalam sesi.");
+                }
+                if (!nama.Contains("BAPENDA"))
+                {
+                    return RedirectToAction("Error", "Home", new { statusCode = 403 });
+                }
+                var model = new Models.KontrolPBB.AnalitikViewVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = ResponseBase.StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = ResponseBase.StatusEnum.Error;
+                response.Message = "⚠️ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+        public IActionResult ShowWilayah(int wilayah, int kec)
+        {
+            try
+            {
+                var model = new Models.KontrolPBB.AnalitikViewVM.ShowWilayah(wilayah, kec);
+                return PartialView($"{URLView}{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = ResponseBase.StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = ResponseBase.StatusEnum.Error;
+                response.Message = "⚠️ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+        public IActionResult ShowSegmentasi(int wilayah, int kec)
+        {
+            try
+            {
+                var model = new Models.KontrolPBB.AnalitikViewVM.ShowSegmentasi(wilayah, kec);
+                return PartialView($"{URLView}{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = ResponseBase.StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = ResponseBase.StatusEnum.Error;
+                response.Message = "⚠️ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+
+    }
+}
--- MonPDReborn/Models/KontrolPBB/AnalitikViewVM.cs
+++ MonPDReborn/Models/KontrolPBB/AnalitikViewVM.cs
@@ -1,4 +1,6 @@
-﻿namespace MonPDReborn.Models.KontrolPBB
+﻿using MonPDLib;
+
+namespace MonPDReborn.Models.KontrolPBB
 {
     public class AnalitikViewVM
     {
@@ -8,10 +10,20 @@
             {
             }
         }
-        public class Show
+        public class ShowWilayah
         {
-            public Show() 
+            public List<ViewModels.DetailWilayah> Data { get; set; } = new();
+            public ShowWilayah(int wilayah, int kec) 
             { 
+                Data = Methods.GetDetailWilayah(wilayah, kec);
+            }
+        }
+        public class ShowSegmentasi
+        {
+            public List<ViewModels.Segmentasi> Data { get; set; } = new();
+            public ShowSegmentasi(int wilayah, int kec)
+            {
+                Data = Methods.GetDataSegmentasi(wilayah, kec);
             }
         }
         public class ViewModels
@@ -35,10 +47,65 @@
                 public decimal Pencapaian => Target == 0 ? 0 : (Realisasi / Target) * 100;
                 public decimal Kontribusi => Realisasi == 0 ? 0 : (Realisasi / Realisasi) * 100;
             }
+            public class DashoboardKinerjaPetugas
+            {
+                public string Petugas { get; set; } = null!;
+                public decimal Capaian { get; set; }
+            }
         }
         public class Methods
         {
-
+            public static List<ViewModels.DetailWilayah> GetDetailWilayah(int wilayah, int kec)
+            {
+                var list = new List<ViewModels.DetailWilayah>()
+                {
+                    new ViewModels.DetailWilayah
+                    {
+                        Wilayah = 5,
+                        NOP = "32.30.20.355.235",
+                        NamaWP = "CV. Simpel",
+                        Penetapan = 250000000,
+                        Realisasi = 150000000,
+                    },
+                    new ViewModels.DetailWilayah
+                    {
+                        Wilayah = 2,
+                        NOP = "32.30.20.355.102",
+                        NamaWP = "CV. Sampel",
+                        Penetapan = 250000000,
+                        Realisasi = 150000000,
+                    }
+                };
+                return list;
+            }
+            public static List<ViewModels.Segmentasi> GetDataSegmentasi(int wilayah, int kec)
+            {
+                var list = new List<ViewModels.Segmentasi>()
+                {
+                    new ViewModels.Segmentasi()
+                    {
+                        KategoriWP = "WP Besar",
+                        JmlObjek = 200,
+                        Target = 30000000000,
+                        Realisasi = 25000000000,
+                    },
+                    new ViewModels.Segmentasi()
+                    {
+                        KategoriWP = "WP Sedang",
+                        JmlObjek = 500,
+                        Target = 15000000000,
+                        Realisasi = 10000000000,
+                    },
+                    new ViewModels.Segmentasi()
+                    {
+                        KategoriWP = "WP Kecil",
+                        JmlObjek = 800,
+                        Target = 5000000000,
+                        Realisasi = 3000000000,
+                    }
+                };
+                return list;
+            }
         }
     }
 }
