8e19ef36b3d442f8ce70cd6bbcbdb53409fbaf3c
BIMOUW <bimonurika@gmail.com>
2025-10-08 11:22:00 +0700

    WS REALTIME PARKIR CCTV

---
+++ CctvRealtimeWs/DataCctv.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonPDLib;
+using MonPDLib.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CctvRealtimeWs
+{
+    public class DataCctv
+    {
+        public class DataOpCctv
+        {
+            public string Nop { get; set; } = "";
+            public string NamaOp { get; set; } = "";
+            public EnumFactory.EVendorParkirCCTV Vendor { get; set; }
+            public string? AccessPoint { get; set; }
+            public string? CctvId { get; set; }
+        }
+
+
+        public static async Task<List<DataOpCctv>> GetDataOpCctvAsync()
+        {
+            using var context = DBClass.GetContext();
+
+            var result = new List<DataOpCctv>();
+
+            // Gunakan ToListAsync agar EF Core menjalankan query secara async
+            var query = await context.MOpParkirCctvs
+                .Include(x => x.MOpParkirCctvTelkoms)
+                .Include(x => x.MOpParkirCctvJasnita)
+                .Where(x => x.IsPasang == 1)
+                .ToListAsync();
+
+            foreach (var item in query)
+            {
+                if (item.Vendor == (int)EnumFactory.EVendorParkirCCTV.Telkom)
+                {
+                    foreach (var cctv in item.MOpParkirCctvTelkoms)
+                    {
+                        result.Add(new DataOpCctv
+                        {
+                            Nop = item.Nop,
+                            NamaOp = item.NamaOp,
+                            Vendor = (EnumFactory.EVendorParkirCCTV)item.Vendor,
+                            AccessPoint = "",
+                            CctvId = cctv.CctvId
+                        });
+                    }
+                }
+                else if (item.Vendor == (int)EnumFactory.EVendorParkirCCTV.Jasnita)
+                {
+                    foreach (var cctv in item.MOpParkirCctvJasnita)
+                    {
+                        result.Add(new DataOpCctv
+                        {
+                            Nop = item.Nop,
+                            NamaOp = item.NamaOp,
+                            Vendor = (EnumFactory.EVendorParkirCCTV)item.Vendor,
+                            AccessPoint = cctv.AccessPoint,
+                            CctvId = cctv.CctvId
+                        });
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
