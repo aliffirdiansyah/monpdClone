b61c36c1b5d54d4b2a1492eabe1a811c42de77da
RaflyRoyan <royanrafly@gmail.com>
2025-10-13 08:53:32 +0700

    Enhance DbOpAccHotel and StrategicViewVM functionality
    - Updated `DbOpAccHotel` class to modify `Nop` property.
    - Added new `DbSet` properties in `ModelContext` for new entities.
    - Configured new entity mappings for `DbMonPbbSumpage` and `MvDbOpAccHotel`.
    - Refactored `StrategicViewVM` to include new properties and remove outdated ones.
    - Updated data retrieval methods to use new database entities.
    - Adjusted display columns in views to reflect new properties.
    - Introduced new classes for `DbMonPbbSumpage` and `MvDbOpAccHotel`.
    - Overall improvements to data handling and alignment with the new schema.

---
+++ MonPDLib/EF/DbMonPbbSumpage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MonPDLib.EF;
+
+[Keyless]
+public partial class DbMonPbbSumpage
+{
+    [Column("KOMPONEN")]
+    [StringLength(18)]
+    [Unicode(false)]
+    public string? Komponen { get; set; }
+
+    [Column("JUMLAH_OBJEK", TypeName = "NUMBER")]
+    public decimal? JumlahObjek { get; set; }
+
+    [Column("TARGET_TAHUNAN", TypeName = "NUMBER")]
+    public decimal? TargetTahunan { get; set; }
+
+    [Column("TOTAL_KETETAPAN", TypeName = "NUMBER")]
+    public decimal? TotalKetetapan { get; set; }
+
+    [Column("TOTAL_REALISASI", TypeName = "NUMBER")]
+    public decimal? TotalRealisasi { get; set; }
+
+    [Column("SELISIH", TypeName = "NUMBER")]
+    public decimal? Selisih { get; set; }
+
+    [Column("CAPAIAN_PERSEN", TypeName = "NUMBER")]
+    public decimal? CapaianPersen { get; set; }
+
+    [Column("PREDIKSI", TypeName = "NUMBER")]
+    public decimal? Prediksi { get; set; }
+
+    [Column("PERSEN_PREDIKSI", TypeName = "NUMBER")]
+    public decimal? PersenPrediksi { get; set; }
+
+    [Column("PERSEN_TARGET", TypeName = "NUMBER")]
+    public decimal? PersenTarget { get; set; }
+}
