6b796b9cb00465198ae140baf2600da35eceda7c
williamjohanp <williamjohanp@gmail.com>
2025-10-13 13:44:17 +0700

    add : Kakulator Reklame kerangka

---
+++ MonPDReborn/Models/Reklame/KakulatorReklameVM.cs
@@ -0,0 +1,35 @@
+﻿using MonPDLib;
+using MonPDLib.Lib;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+
+namespace MonPDReborn.Models.Reklame
+{
+    public class KakulatorReklameVM
+    {
+
+        public class Index
+        {
+            public Index()
+            {
+
+            }
+        }
+
+        public class HitungNilaiSewa
+        {
+            public HitungNilaiSewa()
+            {
+                
+            }
+        }
+
+        public class Method
+        {
+            public static void GetNilaiSewa()
+            {
+
+            }
+        }
+
+    }
+}
