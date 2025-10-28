af20e534a365050e7f24a23cd66b9735b8a851b1
BIMOUW <bimonurika@gmail.com>
2025-10-28 13:47:16 +0700

    update

--- CctvRealtimeWs/JasnitaModels/RekapJasnita2.cs
+++ CctvRealtimeWs/JasnitaModels/RekapJasnita2.cs
@@ -8,6 +8,13 @@ namespace CctvRealtimeWs.JasnitaModels
 {
     public class RekapJasnita2
     {
-
+        public string Id { get; set; } = null!;
+        public string Nop { get; set; } = null!;
+        public string CctvId { get; set; } = null!;
+        public int VendorId { get; set; }
+        public int JenisKend { get; set; }
+        public string? PlatNo { get; set; }
+        public DateTime WaktuMasuk { get; set; }
+        public byte[] ImageData { get; set; }
     }
 }
