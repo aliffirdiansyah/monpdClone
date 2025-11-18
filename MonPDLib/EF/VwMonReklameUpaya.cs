dafd98a836ebd5819db3994636757ce7690e6537
RaflyRoyan <royanrafly@gmail.com>
2025-11-19 04:28:19 +0700

    gg

---
+++ MonPDLib/EF/VwMonReklameUpaya.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MonPDLib.EF;
+
+[Keyless]
+public partial class VwMonReklameUpaya
+{
+    [Column("BULAN", TypeName = "NUMBER")]
+    public decimal? Bulan { get; set; }
+
+    [Column("TAHUN", TypeName = "NUMBER")]
+    public decimal? Tahun { get; set; }
+
+    [Column("UPAYA")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Upaya { get; set; }
+
+    [Column("KETERANGAN")]
+    [StringLength(255)]
+    [Unicode(false)]
+    public string? Keterangan { get; set; }
+
+    [Column("NOR")]
+    [StringLength(12)]
+    [Unicode(false)]
+    public string Nor { get; set; } = null!;
+
+    [Column("NO_FORMULIR")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string? NoFormulir { get; set; }
+
+    [Column("TGL_SKPD", TypeName = "DATE")]
+    public DateTime? TglSkpd { get; set; }
+
+    [Column("KETETAPAN", TypeName = "NUMBER")]
+    public decimal? Ketetapan { get; set; }
+
+    [Column("TOTAL_BAYAR", TypeName = "NUMBER")]
+    public decimal? TotalBayar { get; set; }
+}
---
+++ MonPDReborn/Controllers/Reklame/ReklameTemuanController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonPDReborn.Lib.General;
+using static MonPDReborn.Lib.General.ResponseBase;
+
+namespace MonPDReborn.Controllers.Reklame
+{
+    public class ReklameTemuanController : Controller
+    {
+        string URLView = string.Empty;
+
+        private readonly ILogger<ReklameTemuanController> _logger;
+        private string controllerName => ControllerContext.RouteData.Values["controller"]?.ToString() ?? "";
+        private string actionName => ControllerContext.RouteData.Values["action"]?.ToString() ?? "";
+
+        const string TD_KEY = "TD_KEY";
+        const string MONITORING_ERROR_MESSAGE = "MONITORING_ERROR_MESSAGE";
+        ResponseBase response = new ResponseBase();
+        public ReklameTemuanController(ILogger<ReklameTemuanController> logger)
+        {
+            URLView = string.Concat("../Reklame/", GetType().Name.Replace("Controller", ""), "/");
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                ViewData["Title"] = controllerName;
+                var model = new Models.Reklame.ReklameTemuanVM.Index();
+                return View($"{URLView}{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+        public IActionResult Show(int tahun)
+        {
+            try
+            {
+                var model = new Models.Reklame.ReklameTemuanVM.Show(tahun);
+                return PartialView($"{URLView}_{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+        public IActionResult Detail(int tahun, int bulan, string ket)
+        {
+            try
+            {
+                var model = new Models.Reklame.ReklameTemuanVM.Detail(tahun, bulan, ket);
+                return PartialView($"{URLView}_{actionName}", model);
+            }
+            catch (ArgumentException e)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = e.InnerException == null ? e.Message : e.InnerException.Message;
+                return Json(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Error;
+                response.Message = "⚠ Server Error: Internal Server Error";
+                return Json(response);
+            }
+        }
+    }
+}
